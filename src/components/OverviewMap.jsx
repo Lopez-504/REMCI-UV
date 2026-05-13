4a257875f4d71dbf254529a7d9d3165033112570
Lopez-504 <jorgelopezr8@gmail.com>
2026-05-13 05:07:09 -0400

    debugging images paths

--- src/components/OverviewMap.jsx
+++ src/components/OverviewMap.jsx
@@ -3,8 +3,8 @@ import { MapContainer, TileLayer, Marker, Popup, Tooltip, Circle } from 'react-l
 import 'leaflet/dist/leaflet.css';
 import './overviewMap.css';
 
-//import { STATIONS } from '../constants/stations'
-import { STATIONS } from '../constants/stations-ghp'
+import { STATIONS } from '../constants/stations'
+//import { STATIONS } from '../constants/stations-ghp'
 
 const OverviewMap = () => {
   const [selectedStation, setSelectedStation] = useState(STATIONS[0]);
